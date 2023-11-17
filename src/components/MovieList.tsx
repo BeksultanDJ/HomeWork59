3b5f3a9087be881e02266cec261f2595c0f8cd94
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-17 14:57:10 +0600

    Создал интерфейс, сделал функцию которая создает новый элемент и id для элемента, так же добавил кнопку удаления

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import MovieItem from './MovieItem';
+
+interface Movie {
+    id: number;
+    title: string;
+}
+
+const MovieList: React.FC = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [newMovie, setNewMovie] = useState<string>('');
+
+
+
+    const addMovie = () => {
+        if (newMovie.trim() !== '') {
+            const newMovieItem: Movie = {
+                id: Math.floor(Math.random() * 1000),
+                title: newMovie.trim(),
+            };
+            setMovies([...movies, newMovieItem]);
+            setNewMovie('');
+        }
+    };
+
+
+
+    const deleteMovie = (id: number) => {
+        const updatedMovies = movies.filter(movie => movie.id !== id);
+        setMovies(updatedMovies);
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newMovie}
+                onChange={(e) => setNewMovie(e.target.value)}
+            />
+            <button onClick={addMovie}>Add</button>
+            <ul>
+                {movies.map((movie) => (
+                    <MovieItem
+                        key={movie.id}
+                        movie={movie}
+                        deleteMovie={deleteMovie}
+                    />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default MovieList;
