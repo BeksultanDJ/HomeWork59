19fbe87cbe5d6926a606ae3040063310c9e54077
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-17 18:44:38 +0600

    импортировал jokeDisplay

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MovieList from './components/MovieList';
+import JokeDisplay from './components/JokeDisplay.tsx';
 
 const App = () => {
     return (
         <div className="App">
             <h1>Movies</h1>
             <MovieList />
+            <JokeDisplay/>
         </div>
     );
 };
